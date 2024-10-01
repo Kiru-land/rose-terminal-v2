f5104fb7abf4f9251974930a29edd3994f22de9b
scamilcar <raclimacs@gmail.com>
2024-10-01 03:25:46 +0200

    config

--- config.js
+++ config.js
@@ -1,21 +1,12 @@
 import { Redis } from '@upstash/redis';
 
 // Initialize KV databases with custom configurations
-const clawbackKV = new Redis({
+export const clawbackKV = new Redis({
   url: process.env.KV_PRICES_REST_API_URL,
   token: process.env.KV_PRICES_REST_API_TOKEN,
 });
 
-const pricesKV = new Redis({
+export const pricesKV = new Redis({
   url: process.env.KV_REST_API_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
-
-// Create a config object with the Redis instances
-const config = {
-  clawbackKV,
-  pricesKV,
-};
-
-// Export the configuration and Redis instances
-export default config;
