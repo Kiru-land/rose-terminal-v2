03d8d26c44fce907c39ac755653bbadb8d902582
flyjgh <casselescouilles19@gmail.com>
2024-10-18 19:42:02 +0100

    update metadata, bump version

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -65,7 +65,7 @@ const Intro = ({ asciiLogo, onIntroComplete }) => {
   const [introText, setIntroText] = useState('');
   const [glitterCount, setGlitterCount] = useState(0);
   const [isTypingComplete, setIsTypingComplete] = useState(false);
-  const fullText = "Rose Terminal v0.3.4";
+  const fullText = "Rose Terminal v0.3.5";
 
   const maxGlitters = 100;
 
