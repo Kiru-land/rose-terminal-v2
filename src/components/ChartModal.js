efd65e96f6937813a2b34182217039f4f97861b1
scamilcar <raclimacs@gmail.com>
2024-10-20 05:06:20 +0700

    hehe

--- src/components/ChartModal.js
+++ src/components/ChartModal.js
@@ -52,10 +52,10 @@ const ChartModal = ({ onClose }) => {
           // Format the data as required by Lightweight Charts
           const formattedData = response.data.data
             .map((item) => ({
-              time: new Date(item.timestamp * 1000).toISOString().split('T')[0], // Convert timestamp to 'YYYY-MM-DD' format
+              time: item.timestamp, // Use the timestamp directly
               value: parseFloat(item.price),
             }))
-            .sort((a, b) => a.time.localeCompare(b.time)); // Sort by time
+            .sort((a, b) => a.time - b.time); // Sort by timestamp
           setPriceData(formattedData);
         } else {
           console.error('Invalid data structure received:', response.data);
@@ -85,53 +85,12 @@ const ChartModal = ({ onClose }) => {
       timeScale: {
         timeVisible: true,
         secondsVisible: true,
-        rightOffset: 10,
-        fixLeftEdge: false,
-        fixRightEdge: false,
-        lockVisibleTimeRangeOnResize: false,
-        borderVisible: true,
-        borderColor: '#485c7b',
-        visible: true,
-        tickMarkFormatter: (time, tickMarkType, locale) => {
-          let date;
-          if (typeof time === 'number') {
-            // time is a UTCTimestamp in seconds
-            date = new Date(time * 1000); // Convert seconds to milliseconds
-          } else if (
-            typeof time === 'object' &&
-            time.year !== undefined &&
-            time.month !== undefined &&
-            time.day !== undefined
-          ) {
-            // time is a BusinessDay object
-            date = new Date(Date.UTC(time.year, time.month - 1, time.day));
-          } else {
-            console.error('Unsupported time format:', time);
-            return '';
-          }
-          return date.toLocaleString(locale, {
-            hour12: false,
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-          });
-        },
       },
     });
 
     const lineSeries = chart.addLineSeries({
       color: '#2962FF',
       lineWidth: 2,
-      priceFormat: {
-        type: 'custom',
-        formatter: (price) => {
-          // Format price using scientific notation with two decimal places
-          return price.toExponential(2);
-        },
-      },
     });
 
     lineSeries.setData(priceData);
