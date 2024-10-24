7ec16effa28e3dfc8102021cc4cdff9aa449b9be
scamilcar <raclimacs@gmail.com>
2024-10-24 23:21:51 +0700

    add config

--- src/config.js
+++ src/config.js
@@ -6,6 +6,7 @@ const config = {
         "0xA60489284B69E58781FAfF4C70AA69AE5Ada0f00": 100,
         "0xdBD4D75960ae8A08b53E0B4f679c4Af487256B31": 100
     },
+    SERVER_IP: process.env.SERVER_IP,
 };
 
 export default config;
