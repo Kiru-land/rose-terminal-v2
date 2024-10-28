168e23756a1368c195d2ee1eae17cbfc5efbab9b
flyjgh <casselescouilles19@gmail.com>
2024-10-28 21:08:34 +0000

    rebrand

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -65,7 +65,7 @@ const Intro = ({ asciiLogo, onIntroComplete }) => {
   const [introText, setIntroText] = useState('');
   const [glitterCount, setGlitterCount] = useState(0);
   const [isTypingComplete, setIsTypingComplete] = useState(false);
-  const fullText = "Rose Terminal v0.3.5";
+  const fullText = "Kiru Terminal v0.4.0";
 
   const maxGlitters = 100;
 
