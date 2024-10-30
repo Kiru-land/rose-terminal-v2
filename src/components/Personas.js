cbf9ef35341585ccc30097542793c51880bd6c18
flyjgh <casselescouilles19@gmail.com>
2024-10-30 15:55:40 +0000

    fix selectable text

--- src/components/Personas.js
+++ src/components/Personas.js
@@ -46,6 +46,10 @@ const PersonaText = styled.div`
   background: rgba(0, 0, 0, 0.7);
   padding: 10px;
   border-radius: 5px;
+  user-select: none;
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
 `;
 
 const Personas = ({ isVisible }) => {
